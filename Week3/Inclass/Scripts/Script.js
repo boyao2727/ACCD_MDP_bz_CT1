71965b1fa4136fb1106ebbe774f704505fc05023
boyao2727 <frankiezhang010527@gmail.com>
2025-10-03 14:55:22 -0700

    In-class

--- Week3/Inclass/Scripts/Script.js
+++ Week3/Inclass/Scripts/Script.js
@@ -1,5 +1,7 @@
 let cBox = document.getElementById("colorBox")
 let colorBtn = document.getElementById("changecolor")
+let imgBox = document.getElementById("FrogImage") 
+let imageBtn = document.getElementById("toggleImage")
 
 let assignRandomColor = function()
 {
@@ -9,6 +11,18 @@ let assignRandomColor = function()
     cBox.style.backgroundColor = "rgb(" + rComp + ", " + gComp + ", "+ bComp + ")"
 }
 
+const toggleFrogImage = () =>
+{
+    console.log (imgBox.src)
+    if(imgBox.src.includes("frog1"))
+    {
+        imgBox.src = "assets/frog0.jpg"
+    }
+    else
+    {
+        imgBox.src = "assets/frog1.jpg"
+    }
+}
 
-
+imageBtn.addEventListener("click", toggleFrogImage)
 colorBtn.addEventListener("click", assignRandomColor)  
