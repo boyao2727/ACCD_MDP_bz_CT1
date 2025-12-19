c8d62bbd29e98e77929ad3d4be2c2480a5291acd
boyao2727 <frankiezhang010527@gmail.com>
2025-12-19 10:35:59 -0800

    week 5

--- Week5/homework/sketch.js
+++ Week5/homework/sketch.js
@@ -6,19 +6,33 @@ function setup() {
   colorMode(HSB, TWO_PI, 1, 1)
 
   radius = width * 0.1
-  strokeCap(SQUARE)
+  strokeCap(ROUND)
 }
 
 function draw() {
   background(0)
   noFill()
   strokeWeight(strWeight)
+  let t = millis() * 0.001
+  let basePeriod = 2.2
+  let delayStep = 0.18
+
   for(let i = 0; i < numRings; i++){
-    stroke(color(i*TWO_PI/numRings, 0.9, 0.9))
+    let h = 0.08 * TWO_PI              
+    let s = map(i, 0, numRings-1, 0.45, 0.15)
+    let b = map(i, 0, numRings-1, 0.25, 0.85)
+    stroke(color(h, s, b))
     push()
     translate(width/2, height/2)
-    rotate( sin(millis()*0.001 * (i*0.8+1)))
-    arc(0, 0, radius*2+strWeight*i*2, radius*2+strWeight*i*2, QUARTER_PI+HALF_PI, TWO_PI+QUARTER_PI)
+    let fromOuter = (numRings - 1 - i)           
+    let delay = fromOuter * delayStep            
+    let localT = max(0, t - delay)
+    let period = basePeriod * (1.0 + i * 0.04)
+    let u = (localT % period) / period           
+    let cycle = floor(localT / period)           
+    let ang = (cycle % 2 == 0) ? (u * TWO_PI) : ((1 - u) * TWO_PI)
+    rotate(ang)
+    arc(0, 0, radius*2 + strWeight*i*2, radius*2 + strWeight*i*2, 0, PI)
     pop()
   }
 }
